8f82c695742072c62e017b00184dcbfc7f6189af
User <User@DESKTOP-E32EPOV.Dlink>
2020-04-26 16:52:13 +0200

    First Commit

---
+++ Bamboo/src/main/java/Bamboo/Bamboo/App.java
@@ -0,0 +1,13 @@
+package Bamboo.Bamboo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
